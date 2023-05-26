17cc55db9733f9ad3e906f34d1eca5f03f1a6b9e
Eric Hardy <eric.hardy.29@gmail.com>
2023-05-26 11:16:46 +0200

    bin/blackScreenIdle.sh

---
+++ bin/blackScreenIdle.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+MAX_DELAY=2700
+
+if [ $# != 0 ]
+then
+	DELAY=$1
+else
+	DELAY=180
+fi
+
+gsettings set org.gnome.desktop.session idle-delay $DELAY
+
+spd-say $DELAY -i -95
+
+if [ $DELAY -le 60 ]
+then
+	let SLEEP=($DELAY * 8)
+	sleep $SLEEP
+	gsettings set org.gnome.desktop.session idle-delay $MAX_DELAY
+	spd-say $MAX_DELAY -i -95
+fi
+
+
