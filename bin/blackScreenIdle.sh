7f0e40b5b07987b785b88202180e204b580adaaa
Eric Hardy <eric.hardy.29@gmail.com>
2023-06-26 10:51:50 +0200

    remove spd-say

--- bin/blackScreenIdle.sh
+++ bin/blackScreenIdle.sh
@@ -11,14 +11,14 @@ fi
 
 gsettings set org.gnome.desktop.session idle-delay $DELAY
 
-spd-say $DELAY -i -95
+# spd-say $DELAY -i -95
 
 if [ $DELAY -le 60 ]
 then
 	let SLEEP=($DELAY * 8)
 	sleep $SLEEP
 	gsettings set org.gnome.desktop.session idle-delay $MAX_DELAY
-	spd-say $MAX_DELAY -i -95
+	# spd-say $MAX_DELAY -i -95
 fi
 
 
